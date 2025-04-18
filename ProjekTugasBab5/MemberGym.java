3da0fe34892c933d7139ca2e742aa4ca84f35ec3
PutraAnand <putraanand900@gmail.com>
2025-04-18 15:11:30 +0700

    Bab5  Overriding Dan Tabel

---
+++ ProjekTugasBab5/MemberGym.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProjekTugasBab5;
+
+import ProjekTugasBab4.*;
+import ProjekTugas.*;
+
+/**
+ *
+ * @author Putra Anand Noor R
+ */
+public class MemberGym {
+    private String idMember;
+    private String nama;
+    private String levelMembership;
+
+    public MemberGym(String nama, String idMember, String levelMembership) {
+        this.nama = nama;
+        this.idMember = idMember;
+        this.levelMembership = levelMembership;
+    }
+    // Setter (untuk mengisi data)
+    public void setIdMember(String idMember) {
+        this.idMember = idMember;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setLevelMembership(String levelMembership) {
+        this.levelMembership = levelMembership;
+    }
+
+    // Getter (untuk mengambil data)
+    public String getIdMember() {
+        return idMember;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getLevelMembership() {
+        return levelMembership;
+    }
+    
+    public void upgradeMembership(String newLevel) {
+        this.levelMembership = newLevel;
+        System.out.println("Membership berhasil diupgrade ke level " + newLevel);
+    }
+   public String getMembershipInfo() {
+        return "Informasi Membership:\n"
+             + "Nama: " + nama + "\n"
+             + "ID Membership: " + idMember + "\n"
+             + "Level Membership: " + levelMembership;
+        }
+}
+    
+
---
+++ ProjekTugasBab5/Upgrade.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProjekTugasBab5;
+import ProjekTugasBab4.*;
+import ProjekTugasBab3.*;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author Putra Anand Noor R
+ */
+public class Upgrade extends MemberGym {
+    private List<String> riwayatUpgrade;
+
+     public Upgrade(String nama, String idMember, String levelMembership) {
+        super(nama, idMember, levelMembership);
+        this.riwayatUpgrade = new ArrayList<>();
+    }
+
+    // Override method upgradeMembership
+    @Override
+    public void upgradeMembership(String newLevel) {
+        String riwayat = getLevelMembership() + " -> " + newLevel;
+        riwayatUpgrade.add(riwayat);
+
+        // Panggil method parent
+        super.upgradeMembership(newLevel);
+    }
+
+    // Tampilkan riwayat upgrade
+    public void tampilkanRiwayatUpgrade() {
+        System.out.println("Riwayat Upgrade Membership untuk " + getNama() + ":");
+        for (String riwayat : riwayatUpgrade) {
+            System.out.println(riwayat);
+        }
+    }
+
+    public List<String> getRiwayatUpgrade() {
+        return riwayatUpgrade;
+    }
+}
